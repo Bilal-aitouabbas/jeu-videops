f9db4f559a93bdc1e4e5a653e4ac29f5327108e2
Bilal Ait-ouabbas <bilal.ait-ouabbas@epitech.eu>
2026-04-20 16:15:44 +0200

    fix du pipeline

--- bootstrap-app/bootstrap/index.js
+++ bootstrap-app/bootstrap/index.js
@@ -1,31 +1,32 @@
-const express = require("express");
-const Calculator = require("./modules/calculator.js");
+const express = require('express');
+const Calculator = require('./modules/calculator.js');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const calc = new Calculator.Calculator();
 
-app.get("/", (req, res) => {
-  res.send("[CPT]");
+app.get('/', (req, res) => {
+  res.send('[CPT]');
 });
 
-app.get("/add", (req, res) => {
+app.get('/add', (req, res) => {
   res.send(`${req.query.a} + ${req.query.b} = \
     ${calc.add(parseFloat(req.query.a), parseFloat(req.query.b))}`);
 });
 
-app.get("/sub", (req, res) => {
+app.get('/sub', (req, res) => {
   res.send(`${req.query.a} - ${req.query.b} = \
     ${calc.subtract(parseFloat(req.query.a), parseFloat(req.query.b))}`);
 });
 
-app.get("/mul", (req, res) => {
+app.get('/mul', (req, res) => {
   res.send(`${req.query.a} * ${req.query.b} = \
     ${calc.multiply(parseFloat(req.query.a), parseFloat(req.query.b))}`);
 });
 
-app.get("/div", (req, res) => {
-  const result = calc.divide(parseFloat(req.query.a), parseFloat(req.query.b));
+app.get('/div', (req, res) => {
+  const result = calc.divide(
+      parseFloat(req.query.a), parseFloat(req.query.b));
   if (result == Infinity) {
     res.send(`Cannot divide ${req.query.a} by zero`);
   }
@@ -33,11 +34,10 @@ app.get("/div", (req, res) => {
     ${calc.multiply(parseFloat(req.query.a), parseFloat(req.query.b))}`);
 });
 
-// NEW FEATURE
-app.get("/pow", (req, res) => {
-  res.send(
-    `${req.query.a} ^ ${req.query.b} = ${calc.power(parseFloat(req.query.a), parseFloat(req.query.b))}`,
-  );
+app.get('/pow', (req, res) => {
+  const result = calc.power(
+      parseFloat(req.query.a), parseFloat(req.query.b));
+  res.send(`${req.query.a} ^ ${req.query.b} = ${result}`);
 });
 
 app.listen(PORT, () => {
--- two-ships-passing/cypress.config.js
+++ two-ships-passing/cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://127.0.0.1:5500/two-ships-passing",
+    baseUrl: 'http://localhost:4173',
     supportFile: false,
   },
 })
