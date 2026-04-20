bd31c4afd93bcdc8f9dcc0f0abfd0e67f7d8aa59
Bilal Ait-ouabbas <bilal.ait-ouabbas@epitech.eu>
2026-04-20 16:07:32 +0200

    fix et suppression du fichier en trop en conflit

--- spaceword/eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import globals from "globals"; //fichier fait avec la commande "npm init @eslint/config@latest" en remplissant le questionnaire 
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
